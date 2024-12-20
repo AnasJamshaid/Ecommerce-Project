da5b7796d368d59b3b28a6176f2c71f2a50de36d
Muhammad Anas Jamshaid <anassheikh54211@gmail.com>
2024-12-20 20:09:44 +0500

    nextjs uptade

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
-module.exports = withBundleAnalyzer({
-  // your Next.js config options
-});
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+}
+ 
+module.exports = nextConfig
